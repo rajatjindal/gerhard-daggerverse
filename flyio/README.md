dcdc923d607dea0e92319e042de6d38ac2aa2b15
Gerhard Lazu <gerhard@lazu.ch>
2024-03-08 20:25:51 +0000

    flyio: Add short & long description
    + bump to latest flyctl
    + use secrets from envs safely
    + test with 0.10.1
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.ch>

--- flyio/README.md
+++ flyio/README.md
@@ -1,3 +1,5 @@
+[![tested-with-dagger-version](https://img.shields.io/badge/Tested%20with%20dagger-0.10.1-success?style=for-the-badge)](https://github.com/dagger/dagger/releases/tag/v0.10.1)
+
 # Dagger Fly.io Module
 
 ![dagger-min-version](https://img.shields.io/badge/dagger%20version-v0.9.2-green)
@@ -6,7 +8,7 @@ Manage apps on <https://fly.io>
 
 ## Deploy
 
-Assumes that there is a valid `fly.toml` at the `--dir` path:
+Assumes there is a valid `fly.toml` at the `--dir` path:
 
 ```sh
 dagger call deploy --dir . --token "$YOUR_FLYIO_AUTH_TOKEN"
--- flyio/main.go
+++ flyio/main.go
@@ -1,3 +1,7 @@
+// Manage apps on https://fly.io
+//
+// Currently only deploys.
+// Assumes there is a valid fly.toml in the path provided to --dir.
 package main
 
 import (
@@ -7,12 +11,12 @@ import (
 
 const (
 	// https://hub.docker.com/r/flyio/flyctl/tags
-	version = "0.1.115"
+	version = "0.2.13"
 )
 
 type Flyio struct{}
 
-// Example usage: "dagger call deploy --dir . --token "$YOUR_FLYIO_AUTH_TOKEN"
+// Example usage: "dagger call deploy --dir . --token env:FLY_API_TOKEN"
 func (m *Flyio) Deploy(ctx context.Context, dir *Directory, token *Secret) (string, error) {
 	return dag.Container().
 		From(fmt.Sprintf("flyio/flyctl:v%s", version)).
