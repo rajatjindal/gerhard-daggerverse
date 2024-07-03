61fbe35953bbc2fc50da69d937ce4463172b44e9
Gerhard Lazu <gerhard@lazu.ch>
2024-07-03 14:49:53 +0100

    flyio: Add app creation
    Also bump to the latest Dagger v0.11.9 & flyctl 0.2.79
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.ch>

--- flyio/README.md
+++ flyio/README.md
@@ -1,15 +1,28 @@
-[![tested-with-dagger-version](https://img.shields.io/badge/Tested%20with%20dagger-0.10.1-success?style=for-the-badge)](https://github.com/dagger/dagger/releases/tag/v0.10.1)
+[![tested-with-dagger-version](https://img.shields.io/badge/Tested%20with%20dagger-0.11.9-success?style=for-the-badge)](https://github.com/dagger/dagger/releases/tag/v0.10.1)
 
 # Dagger Fly.io Module
 
-![dagger-min-version](https://img.shields.io/badge/dagger%20version-v0.9.2-green)
+![dagger-min-version](https://img.shields.io/badge/dagger%20version-v0.11.9-green)
 
 Manage apps on <https://fly.io>
+
+```sh
+dagger functions
+Name     Description
+create   Creates app - required for deploy to work: `dagger call ... create --app=gostatic-example-2024-0703`
+deploy   Deploys app from current directory: `dagger call ... deploy --dir=hello-static`
+```
+
+## Create
+
+```sh
+dagger call --org=personal --token=env:FLY_API_TOKEN create --app=gostatic-example-2024-07-03
+```
 
 ## Deploy
 
 Assumes there is a valid `fly.toml` at the `--dir` path:
 
 ```sh
-dagger call deploy --dir . --token "$YOUR_FLYIO_AUTH_TOKEN"
+dagger call --org=personal --token=env:FLY_API_TOKEN deploy --dir=hello-static
 ```
--- flyio/main.go
+++ flyio/main.go
@@ -7,22 +7,79 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 const (
 	// https://hub.docker.com/r/flyio/flyctl/tags
-	version = "0.2.13"
+	latestVersion = "0.2.79"
 )
 
-type Flyio struct{}
+type Flyio struct {
+	// +private
+	Container *Container
+	// +private
+	Version string
+	// +private
+	Org string
+}
 
-// Example usage: "dagger call deploy --dir . --token env:FLY_API_TOKEN"
-func (m *Flyio) Deploy(ctx context.Context, dir *Directory, token *Secret) (string, error) {
-	return dag.Container().
-		From(fmt.Sprintf("flyio/flyctl:v%s", version)).
+func New(
+	// fly auth token: `--token=env:FLY_API_TOKEN`
+	token *Secret,
+
+	// Fly.io org where all operations will run in: `--org=personal`
+	org string,
+
+	// flyctl version to use: `--version=0.2.79`
+	//
+	// +optional
+	version string,
+
+	// Custom container to use as the base container
+	//
+	// +optional
+	container *Container,
+) *Flyio {
+	if container == nil {
+		if version == "" {
+			version = latestVersion
+		}
+
+		container = dag.Container().
+			From(fmt.Sprintf("flyio/flyctl:v%s", version)).
+			WithSecretVariable("FLY_API_TOKEN", token).
+			WithEnvVariable("CACHE_BUSTED_AT", time.Now().String())
+	}
+
+	return &Flyio{
+		Container: container,
+		Version:   version,
+		Org:       org,
+	}
+}
+
+// Deploys app from current directory: `dagger call ... deploy --dir=hello-static`
+func (m *Flyio) Deploy(
+	ctx context.Context,
+	// App directory - must contain `fly.toml`
+	dir *Directory,
+) (string, error) {
+	return m.Container.
 		WithMountedDirectory("/app", dir).
 		WithWorkdir("/app").
-		WithSecretVariable("FLY_API_TOKEN", token).
 		WithExec([]string{"deploy"}).
+		Stdout(ctx)
+}
+
+// Creates app - required for deploy to work: `dagger call ... create --app=gostatic-example-2024-07-03`
+func (m *Flyio) Create(
+	ctx context.Context,
+	// App name: `--app=myapp-2024-07-03`
+	app string,
+
+) (string, error) {
+	return m.Container.
+		WithExec([]string{"apps", "create", app, "--org", m.Org}).
 		Stdout(ctx)
 }
