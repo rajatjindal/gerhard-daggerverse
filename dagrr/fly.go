3fb884eb84c0f0c8be8a26ab9f4700c0f9f85014
Gerhard Lazu <gerhard@lazu.ch>
2024-08-19 17:23:04 +0100

    Allow disk & size customization when deploying Dagger on Fly.io
        dagger call flyio --token=env:FLY_API_TOKEN --disk=500GB --size=performance-16x --org=dagger
    
    Also allow writing the manifest locally - useful before deploying.
    
        dagger call on-flyio --token=env:FLY_API_TOKEN manifest file --path=fly.toml export --path=fly.toml
    
        # somewhat similar to, but does not require the app to be deployed
        flyctl config save
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.ch>

---
+++ dagrr/fly.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"dagger/dagrr/internal/dagger"
+	"fmt"
+	"strings"
+)
+
+type DagrrFly struct {
+	// +private
+	Dagrr *Dagrr
+	// +private
+	Flyio *dagger.Flyio
+}
+
+// App manifest: dagger call on-flyio --token=env:FLY_API_TOKEN manifest file --path=fly.toml export --path=fly.toml
+func (m *DagrrFly) Manifest(
+	// Disk size in GB
+	//
+	// +optional
+	// +default="100GB"
+	disk string,
+
+	// VM size, see https://fly.io/docs/about/pricing/#compute
+	//
+	// +optional
+	// +default="performance-2x"
+	size string,
+) *dagger.Directory {
+	mount := strings.ReplaceAll(m.Dagrr.App, "-", "_")
+	toml := fmt.Sprintf(`# https://fly.io/docs/reference/configuration/
+
+app = "%s"
+
+kill_signal = "SIGINT"
+kill_timeout = 30
+
+[build]
+  image = "registry.dagger.io/engine:v%s"
+
+[mounts]
+  source = "%s"
+  destination = "/var/lib/dagger"
+  initial_size = "%s"
+
+[processes]
+  dagger = "--addr unix:///var/run/buildkit/buildkitd.sock --addr tcp://0.0.0.0:2345"
+
+[checks]
+  [checks.http]
+    grace_period = "3s"
+    interval = "2s"
+    port = 2345
+    timeout = "1s"
+    type = "tcp"
+
+[[services]]
+  internal_port = 2345
+  protocol = "tcp"
+  auto_stop_machines = 'off'
+  auto_start_machines = true
+  min_machines_running = 1
+  processes = ["dagger"]
+
+  [[services.ports]]
+    handlers = ["http"]
+    port = 2345
+
+[[vm]]
+  size = "%s"
+	`, m.Dagrr.App, m.Dagrr.Version, mount, disk, size)
+
+	return dag.Directory().WithNewFile("fly.toml", toml)
+}
+
+// Deploy with default manifest: dagger call on-flyio --token=env:FLY_API_TOKEN deploy
+func (m *DagrrFly) Deploy(
+	// +optional
+	dir *dagger.Directory,
+) (string, error) {
+	ctx := context.Background()
+
+	create, err := m.Flyio.Create(ctx, m.Dagrr.App)
+	if err != nil {
+		return create, err
+	}
+
+	if dir == nil {
+		dir = m.Manifest("100GB", "performance-2x")
+	}
+
+	return m.Flyio.Deploy(ctx, dir)
+}
