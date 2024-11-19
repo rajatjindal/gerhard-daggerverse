37cc457a10c749640ef4a2a3da83c5e50f8d9120
Gerhard Lazu <gerhard@lazu.ch>
2024-11-19 13:24:06 +0000

    sysi: Bump to Dagger v0.14.0
    Signed-off-by: Gerhard Lazu <gerhard@lazu.ch>

--- sysi/main.go
+++ sysi/main.go
@@ -7,24 +7,26 @@ package main
 
 import (
 	"context"
+	"dagger/sysi/internal/dagger"
 	"time"
 )
 
 type Sysi struct{}
 
-// https://github.com/dylanaraps/neofetch
+// https://github.com/dylanaraps/neofetch `dagger call neofetch`
 func (m *Sysi) Neofetch(ctx context.Context) (string, error) {
 	return m.apk("neofetch").WithExec([]string{"neofetch"}).Stdout(ctx)
 }
 
-// https://github.com/fastfetch-cli/fastfetch
+// https://github.com/fastfetch-cli/fastfetch `dagger call fastfetch`
 func (m *Sysi) Fastfetch(ctx context.Context) (string, error) {
 	return m.apk("fastfetch").WithExec([]string{"fastfetch", "--pipe", "false"}).Stdout(ctx)
 }
 
-func (m *Sysi) apk(pkg string) *Container {
+func (m *Sysi) apk(pkg string) *dagger.Container {
+	// https://hub.docker.com/_/alpine/tags
 	return dag.Container().
-		From("alpine:20240606@sha256:166710df254975d4a6c4c407c315951c22753dcaa829e020a3fd5d18fff70dd2").
+		From("alpine:3.20.3@sha256:1e42bbe2508154c9126d48c2b8a75420c3544343bf86fd041fb7527e017a4b4a").
 		WithExec([]string{"apk", "add", pkg}).
 		WithEnvVariable("CACHE_BUSTED_AT", time.Now().String())
 }
